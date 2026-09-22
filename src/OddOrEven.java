5fe194a3e81b6c2fdd07dafed92ccf48faa78c32
Gabriel Rhys Pelobello <320895786+GabrielRhys00@users.noreply.github.com>
2026-09-22 15:38:26 +0800

    Refactor OddOrEven class for better exception handling

--- src/OddOrEven.java
+++ src/OddOrEven.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class OddOrEven {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
 
         int result;
