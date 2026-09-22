11895c2051797225f0352b7985348ff8a3597a00
Gabriel Rhys!! <320895786+GabrielRhys00@users.noreply.github.com>
2026-09-22 14:47:45 +0800

    Refactor ordering system in TopGlobalYearner
    Fixed issues with cash handling and input validation.

--- src/TopGlobalYearner.java
+++ src/TopGlobalYearner.java
@@ -34,10 +34,11 @@ public class TopGlobalYearner {
                     System.out.print("Cash:   ");
                     int brgrCash = bby.nextInt();
                     change = brgrCash - total;
-                    if (change >= total) {
+
+                    if (brgrCash >= total) {
                         System.out.println("Change: " + change);
-                    } else if (change < total) {
-                        System.out.println("BAWAL UTANG!!   \nYour change:  " + change);
+                    } else if (brgrCash < total) {
+                        System.out.println("\nBAWAL UTANG!!   \nYour change:  " + change);
                     } else {
                         System.out.println("Invalid input. Please check your input.");
                     }
@@ -52,10 +53,11 @@ public class TopGlobalYearner {
                     System.out.print("Cash:   ");
                     int friesCash = bby.nextInt();
                     change = friesCash - total;
-                    if (change >= total) {
+
+                    if (friesCash >= total) {
                         System.out.println("Change: " + change);
-                    } else if (change < total) {
-                        System.out.println("BAWAL UTANG!!   \nYour change" + change);
+                    } else if (friesCash < total) {
+                        System.out.println("\nBAWAL UTANG!!   \nYour change" + change);
                     } else {
                         System.out.println("Invalid input. Please check your input.");
                     }
@@ -70,10 +72,11 @@ public class TopGlobalYearner {
                     System.out.print("Cash:   ");
                     int stapeghiCash = bby.nextInt();
                     change = stapeghiCash - total;
-                    if (change >= total) {
+
+                    if (stapeghiCash >= total) {
                         System.out.println("Change: " + change);
-                    } else if {
-                        System.out.println("BAWAL UTANG!!   \nYour change" + change);
+                    } else if (stapeghiCash < total) {
+                        System.out.println("\nBAWAL UTANG!!   \nYour change" + change);
                     } else {
                         System.out.println("Invalid input. Please check your input.");
                     }
@@ -88,10 +91,11 @@ public class TopGlobalYearner {
                     System.out.print("Cash:   ");
                     int drinksCash = bby.nextInt();
                     change = drinksCash - total;
-                    if (change >= total) {
+
+                    if (drinksCash >= total) {
                         System.out.println("Change: " + change);
-                    } else if {
-                        System.out.println("BAWAL UTANG!!   \nYour change" + change);
+                    } else if (drinksCash < total) {
+                        System.out.println("\nBAWAL UTANG!!   \nYour change" + change);
                     } else {
                         System.out.println("Invalid input. Please check your input.");
                     }
@@ -102,66 +106,66 @@ public class TopGlobalYearner {
             }
 
         }
-    }
 
-    /*
-     * for (int i = 0; i <= 5; i++) {
-     * System.out.println("========= MIDTERM GRADE =========");
-     * System.out.print("Enter student's name:    ");
-     * String pogi = bby.nextLine();
-     * System.out.print("Enter Quiz#1:    ");
-     * int mahal = bby.nextInt();
-     * System.out.print("Enter Quiz#2:    ");
-     * int mo = bby.nextInt();
-     * System.out.print("Enter Midterm Exam Grade:    ");
-     * int bako = bby.nextInt();
-     * int hoy = (mahal + mo + bako) / 3;
-     * if (hoy >= 75 && hoy <= 100) {
-     * System.out.println("\nYour Average Midterm Grade is " + hoy);
-     * 
-     * System.out.println("Remarks: PASSED");
-     * } else if (hoy >= 0 && hoy <= 74) {
-     * System.out.println("\nYour Average Midterm Grade is " + hoy);
-     * System.out.println("Remarks: Failed");
-     * } else {
-     * System.out.println("Invalid input. Please check your input.");
-     * }
-     * 
-     * System.out.println("\n========= FINAL GRADE =========");
-     * System.out.print("Enter Quiz#1:    ");
-     * int palambing = bby.nextInt();
-     * System.out.print("Enter Quiz#2:    ");
-     * int po = bby.nextInt();
-     * System.out.print("Enter Final Exam Grade:    ");
-     * int ako = bby.nextInt();
-     * int baby = (palambing + po + ako) / 3;
-     * if (baby >= 75 && baby <= 100) {
-     * System.out.println("\nYour Average Final Grade is " + baby);
-     * System.out.println("Remarks: PASSED");
-     * } else if (baby >= 0 && baby <= 74) {
-     * System.out.println("\nYour Average Final Grade is " + baby);
-     * System.out.println("Remarks: Failed");
-     * } else {
-     * System.out.println("Invalid input. Please check your input.");
-     * }
-     * 
-     * int psst = (hoy + baby) / 2;
-     * if (psst >= 75 && psst <= 100) {
-     * System.out.println("\n=====================================");
-     * System.out.println("Student's Name: " + pogi);
-     * System.out.println("Your Overall Grade is " + psst);
-     * System.out.println("Remarks: PASSED");
-     * } else if (psst >= 0 && psst <= 74) {
-     * System.out.println("\n======================================");
-     * System.out.println("Student's Name: " + pogi);
-     * System.out.println("Your Overall Grade is " + psst);
-     * System.out.println("Remarks: Failed");
-     * } else {
-     * System.out.println("Invalid input. Please chesck your input.");
-     * }
-     * System.out.println("====================================\n\n");
-     * 
-     * }
-     */
-   
+        /*
+         * for (int i = 0; i <= 5; i++) {
+         * System.out.println("========= MIDTERM GRADE =========");
+         * System.out.print("Enter student's name:    ");
+         * String pogi = bby.nextLine();
+         * System.out.print("Enter Quiz#1:    ");
+         * int mahal = bby.nextInt();
+         * System.out.print("Enter Quiz#2:    ");
+         * int mo = bby.nextInt();
+         * System.out.print("Enter Midterm Exam Grade:    ");
+         * int bako = bby.nextInt();
+         * int hoy = (mahal + mo + bako) / 3;
+         * if (hoy >= 75 && hoy <= 100) {
+         * System.out.println("\nYour Average Midterm Grade is " + hoy);
+         * 
+         * System.out.println("Remarks: PASSED");
+         * } else if (hoy >= 0 && hoy <= 74) {
+         * System.out.println("\nYour Average Midterm Grade is " + hoy);
+         * System.out.println("Remarks: Failed");
+         * } else {
+         * System.out.println("Invalid input. Please check your input.");
+         * }
+         * 
+         * System.out.println("\n========= FINAL GRADE =========");
+         * System.out.print("Enter Quiz#1:    ");
+         * int palambing = bby.nextInt();
+         * System.out.print("Enter Quiz#2:    ");
+         * int po = bby.nextInt();
+         * System.out.print("Enter Final Exam Grade:    ");
+         * int ako = bby.nextInt();
+         * int baby = (palambing + po + ako) / 3;
+         * if (baby >= 75 && baby <= 100) {
+         * System.out.println("\nYour Average Final Grade is " + baby);
+         * System.out.println("Remarks: PASSED");
+         * } else if (baby >= 0 && baby <= 74) {
+         * System.out.println("\nYour Average Final Grade is " + baby);
+         * System.out.println("Remarks: Failed");
+         * } else {
+         * System.out.println("Invalid input. Please check your input.");
+         * }
+         * 
+         * int psst = (hoy + baby) / 2;
+         * if (psst >= 75 && psst <= 100) {
+         * System.out.println("\n=====================================");
+         * System.out.println("Student's Name: " + pogi);
+         * System.out.println("Your Overall Grade is " + psst);
+         * System.out.println("Remarks: PASSED");
+         * } else if (psst >= 0 && psst <= 74) {
+         * System.out.println("\n======================================");
+         * System.out.println("Student's Name: " + pogi);
+         * System.out.println("Your Overall Grade is " + psst);
+         * System.out.println("Remarks: Failed");
+         * } else {
+         * System.out.println("Invalid input. Please chesck your input.");
+         * }
+         * System.out.println("====================================\n\n");
+         * 
+         * }
+         */
+        bby.close();
+    }
 }
